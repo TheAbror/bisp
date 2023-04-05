304a0696bc1dc2b8211849aa406a5ea584d82c9e
Elbarto <oyjddkvj@gmail.com>
2023-04-05 17:58:49 +0500

    ready

--- lib/presention/subjects/quzzes/solve_quizzes.dart
+++ lib/presention/subjects/quzzes/solve_quizzes.dart
@@ -1,7 +1,6 @@
 import 'package:eduninjav2/core/constants/values/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class SolveQuizzes extends StatefulWidget {
   const SolveQuizzes({super.key});
@@ -13,16 +12,13 @@ class SolveQuizzes extends StatefulWidget {
 class _SolveQuizzesState extends State<SolveQuizzes> {
   int count = 0;
 
-  void showToast() {
-    Fluttertoast.showToast(
-      msg: "Required level is ${count + 1}",
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      timeInSecForIosWeb: 1,
-      backgroundColor: Colors.red,
-      textColor: Colors.white,
-      fontSize: 14.sp,
-    );
+  final GlobalKey<FormState> _form = GlobalKey();
+
+  final String answer = 'oxygen';
+  String enteredValue = '';
+
+  bool isCorrect() {
+    return enteredValue == answer;
   }
 
   @override
@@ -33,54 +29,115 @@ class _SolveQuizzesState extends State<SolveQuizzes> {
         child: Stack(
           fit: StackFit.expand,
           children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 20.h),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: AppColors.primaryColor, width: 2.w),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ListView.builder(
-                  itemCount: 1,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 5.h),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Fill in the gaps',
-                            style: TextStyle(
-                              fontSize: 28.sp,
-                              fontWeight: FontWeight.bold,
-                              color: AppColors.primaryColor,
-                              decoration: TextDecoration.underline,
-                            ),
-                          ),
-                          SizedBox(height: 40.h),
-                          Text(
-                            '1. Protist, fungi, plants,and animals are _________ organisms.',
-                            style: TextStyle(fontSize: 28.sp),
-                          ),
-                          SizedBox(height: 20.h),
-                          //TODO Eukoryotic
-                          Container(
-                            margin: EdgeInsets.symmetric(horizontal: 10.w),
-                            child: const TextField(),
-                          ),
-                          SizedBox(height: 20.h),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                minimumSize: Size(300, 50.h), backgroundColor: Colors.red.shade900),
-                            onPressed: () {
-                              showToast();
-                            },
-                            child: const Text('Check Results'),
-                          ),
-                        ],
+            Form(
+              key: _form,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10.w, vertical: 20.h),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: AppColors.primaryColor, width: 2.w),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 5.h),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Fill in the gaps',
+                        style: TextStyle(
+                          fontSize: 28.sp,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primaryColor,
+                          decoration: TextDecoration.underline,
+                        ),
                       ),
-                    );
-                  }),
+                      SizedBox(height: 40.h),
+                      Text(
+                        '1. Cell membranes are semipermeable, allowing small molecules such as ______, carbon dioxide and water to pass through while restricting the movement of larger molecules and charged  particles such as ions',
+                        style: TextStyle(fontSize: 28.sp),
+                      ),
+                      SizedBox(height: 20.h),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10.w),
+                        child: TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              enteredValue = value;
+                            });
+                          },
+                          decoration: const InputDecoration(
+                            hintText: 'Enter the Value',
+                          ),
+                          validator: (text) {
+                            if (text?.isNotEmpty != true) {
+                              return 'Value can not be empty';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 20.h),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(300, 50.h), backgroundColor: Colors.red.shade900),
+                          onPressed: isCorrect()
+                              ? () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      title: const Text(
+                                        'Correct!',
+                                        style: TextStyle(fontWeight: FontWeight.bold),
+                                      ),
+                                      content: const Text('You have earned 5 points)'),
+                                      actions: <Widget>[
+                                        ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              minimumSize: Size(double.infinity, 50.h),
+                                              backgroundColor: Colors.red.shade900),
+                                          child: const Text('OK'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                }
+                              : () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      title: const Text(
+                                        'Incorrect!',
+                                        style: TextStyle(fontWeight: FontWeight.bold),
+                                      ),
+                                      content: const Text('Please try again'),
+                                      actions: <Widget>[
+                                        ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              minimumSize: Size(double.infinity, 50.h),
+                                              backgroundColor: Colors.red.shade900),
+                                          child: const Text('OK'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                          child: const Text('Check Results')
+
+                          // onPressed: () {
+                          //   showToast();
+                          // },
+                          ),
+                    ],
+                  ),
+                ),
+              ),
             ),
             Positioned(
               right: 7.w,
